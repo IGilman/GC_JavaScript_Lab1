4bfb536c7d6c07c243b323f5ab026e0454644a37
isaac <gilman.isaac@gmail.com>
2020-04-28 08:56:30 -0400

    extra work added - math is wrong somewhere and two JS files still TODO

---
+++ scripttwo.js
@@ -0,0 +1,27 @@
+let print = null;
+let totalhours;
+let overtimehours;
+let timeandhalf;
+let overtimewage;
+
+
+function myFunction() {
+    let wage = document.getElementById("wage").value;
+    let hours = document.getElementById("hours").value;
+
+    if (hours > 40) {
+        for (let i = 40; i <= hours; i++) {
+            totalhours = i;
+        }
+        overtimehours = totalhours - 40;
+        timeandhalf = wage * 1.5;
+        overtimewage = overtimehours * timeandhalf;
+        print = wage * hours + overtimewage;
+        document.getElementById("printout").innerHTML = print;
+    }
+    else {
+        print = wage * hours;
+        document.getElementById("printout").innerHTML = print;
+    }
+
+}
